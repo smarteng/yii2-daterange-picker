d5664cc647b24441312588c00acad2e37cfb8ed0
Smarteng <smarteng@qq.com>
2016-08-05 18:26:55 +0800

    修改 js function 报错

--- src/DateRangePicker.php
+++ src/DateRangePicker.php
@@ -4,6 +4,7 @@ namespace jerryteng\daterangepicker;
 use yii\helpers\Json;
 use yii\base\Widget;
 use yii\web\View;
+use yii\web\JsExpression;
 
 /**
  * DateRangePicker renders a DatePicker range input.
@@ -37,6 +38,8 @@ class DateRangePicker extends Widget
         DateRangePickerAsset::register($this->view);
 
         $key = __CLASS__ . '#' . $this->id;
+        $this->options['success'] =  new JsExpression($this->options['success']);
+        
         $jsOptions = Json::encode($this->options);
         $javascript = "var dateRange = new pickerDateRange('".$this->id."', ".$jsOptions.");";
         $this->view->registerJs($javascript, View::POS_READY, $key);
